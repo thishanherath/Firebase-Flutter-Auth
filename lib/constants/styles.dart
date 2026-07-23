f76546b8a4c6418289133f62761073ac8e570038
Thishan Herath <diniduthishan9@gmail.com>
2026-07-23 11:06:39 +0530

    login page

---
+++ lib/constants/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/colors.dart';
+
+
+const TextStyle descriptionStyle = TextStyle(
+  fontSize: 12,
+  color: textLight,
+  fontWeight: FontWeight.w200,
+);
--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/constants/description.dart';
+import 'package:flutter_application_1/constants/styles.dart';
 import 'package:flutter_application_1/services/auth.dart';
 
 class Sign_In extends StatefulWidget {
@@ -13,22 +16,164 @@ class _Sign_InState extends State<Sign_In> {
   //ref for the AuthService class
   final AuthServices _auth = AuthServices();
 
+  //for key
+  final _formKey = GlobalKey<FormState>();
+
+  //email passowrd status
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sign In')),
-      body: ElevatedButton(
-        onPressed: () async {
-          dynamic result = await _auth.signInAnon();
-          if (result == Null) {
-            print("error signing in");
-          } else {
-            print("signed in annonymously");
-            print(result.uid);
-          }
-        },
-        child: const Text('Sign In Anonymously'),
+      backgroundColor: bgBlack,
+      appBar: AppBar(
+        backgroundColor: bgBlack,
+        title: const Text('Sign In'),
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 10),
+          child: Column(
+            children: [
+              Text(description, style: descriptionStyle),
+
+              Center(child: Image.network('https://img.icons8.com/?size=100&id=TjTdwE5q2APd&format=png&color=000000', width: 186, height: 186)),
+
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) =>
+                          value!.isEmpty ? 'Enter an email' : null,
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      validator: (value) => value!.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                    ),
+                    //Google
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Login with social accounts",
+                      style: descriptionStyle,
+                    ),
+
+                    GestureDetector(
+                      onTap: () async {},
+                      child: Center(
+                        child: Image.network(
+                          'https://img.icons8.com/?size=100&id=17949&format=png&color=000000',
+                          width: 50,
+                          height: 50,
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    //register
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?", style: descriptionStyle),
+                        const SizedBox(width: 10),
+                        GestureDetector(
+                          onTap: () {
+                            // Navigate to the registration screen
+                          },
+                          child: Text(
+                            "REGISTER",
+                            style: TextStyle(
+                              color: mainBlue,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    GestureDetector(
+                      onTap: () async {
+                        dynamic result = await _auth.signInAnon();
+                        if (result == null) {
+                          print("error signing in");
+                        } else {
+                          print("signed in annonymously");
+                          print(result.uid);
+                        }
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: bgBlack,
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(color: mainYellow, width: 5),
+                        ),
+                        child: Center(child: const Text("LOG IN", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500))),
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    GestureDetector(
+                      onTap: () async {
+                        dynamic result = await _auth.signInAnon();
+                        if (result == null) {
+                          print("error signing in");
+                        } else {
+                          print("signed in annonymously");
+                          print(result.uid);
+                        }
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: bgBlack,
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(color: mainYellow, width: 5),
+                        ),
+                        child: Center(child: const Text("LOG AS GUEST", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500))),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+
+
+// ElevatedButton(
+//         onPressed: () async {
+//           dynamic result = await _auth.signInAnon();
+//           if (result == Null) {
+//             print("error signing in");
+//           } else {
+//             print("signed in annonymously");
+//             print(result.uid);
+//           }
+//         },
+//         child: const Text('Sign In Anonymously'),
+//       ),
